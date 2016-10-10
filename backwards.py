be74440ba833f489e5d0995534e2677a01093c0a
MashaPo <ponomarevamawa@gmail.com>
2016-10-10 18:42:36 +0300

    Add files via upload

---
+++ backwards.py
@@ -0,0 +1,15 @@
+while True:
+    word = input('напишите слово или "конец"\n')
+    if word == 'конец':
+        print('До свидания')
+        break
+    else:
+        count = -1
+        new_word = ''
+        while count  > -len(word) - 1:
+            new_word = new_word + word[count]
+            count -= 1
+        print(new_word)
+
+
+    
