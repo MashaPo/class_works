8e11f6c9bf4bc3bb717b7d1472f0281b78d5e687
MashaPo <ponomarevamawa@gmail.com>
2016-10-10 15:05:49 +0300

    Update morph.py

--- morph.py
+++ morph.py
@@ -1,24 +1,22 @@
-#распознаем существительные
-vocF = 'ая'
-vocN = 'ое'
+#распознаем существительные в начальной форме (грубо). Для F и N задаем концы строк, M по остаточному признаку
+F_endings = ['а', 'я', 'сть', 'шь']
+N_endings = ['о', 'е']
 while True:
     inpt = input('введите слово или напишите "конец"\n')    
     if inpt == 'конец':
-        print('конец - noun masc')
         break
-    elif inpt.endswith('ость') or inpt.endswith('шь'):
-        print('noun fem')
+
     else:
-        for v in vocF:
-            if inpt.endswith(v):
-                print('noun fem')
-                break
-            else:
-                for v in vocN:
-                    if inpt.endswith(v):
-                        print('noun N')
-                        break
-                    else:
-                        print('noun M')
-        continue
-   
+        found_it = False
+        while found_it == False:
+            for f in F_endings:
+                if inpt.endswith(f):
+                    print('noun F')
+                    found_it = True
+            for n in N_endings:
+                if inpt.endswith(n):
+                    print('noun N')
+                    found_it = True
+            if found_it == False:
+                print('noun M')
+                found_it = True
