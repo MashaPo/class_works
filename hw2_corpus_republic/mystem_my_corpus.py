e1e2f4836a2123aac5764d28c05c4d372fc39b68
MashaPo <ponomarevamawa@gmail.com>
2017-03-03 16:15:12 +0300

    скрипт для обработки готового корпуса mystem'ом
    mystem.exe должен лежать в корне проекта со скриптом и папкой corpus. Создает папку morph_corpus с идентичной структурой

---
+++ hw2_corpus_republic/mystem_my_corpus.py
@@ -0,0 +1,15 @@
+﻿import os
+from subprocess import call
+#возвращает тройки вида директория-список папок-список файлов
+dirs = os.walk('corpus')
+for dir in dirs:
+    #дублируем структуру в каталог morph_corpus
+    new_dir = 'morph_' + dir[0] + '/'
+    os.makedirs(os.path.dirname(new_dir), exist_ok=True)
+    files = dir[2]
+    for file in files:
+        input_file = dir[0] + '/' + file
+        output_file = new_dir + file
+        #команду передаем на консоль (указываем относительный путь, бинарник должен лежать в папке проекта)
+        mystem_call = 'mystem.exe -id %s %s' % (input_file,output_file)
+        call(mystem_call)
